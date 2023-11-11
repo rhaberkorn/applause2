93596df1755400c3eac5686668fd4a5d5728b474
Robin Haberkorn <robin.haberkorn@googlemail.com>
2023-11-12 00:44:04 +0300

    documented how to run Applause in Jupyter Consoles and Notebooks
    * This is at the very least cool to have and will be worthwile to write about in the dissertation.
    * Added ilua-wrapper.sh script which can be used as the ILua interpreter and/or can be symlinked to `lua` in PATH,
      so ILua will pick up Applause even in Notebooks without any additional tweaks.

---
+++ ilua-wrapper.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This wrapper exists, so you can pass additional arguments to Applause when
+# using it as an ILua interpreter.
+# Also, it's useful as you do not have to change the directory before invoking ILua.
+# Thirdly, if you create a symbolic link to lua in a Python environment of ILua, this invoke
+# Applause by default and it will even work with Jupyter Notebooks without further tweaks.
+cd $(dirname $(readlink -m "$0"))
+exec ./applause -o 2 "$@"
