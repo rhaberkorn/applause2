90d530fe1067ce9080c58fddc936904db90e361e
Robin Haberkorn <robin.haberkorn@googlemail.com>
2024-05-20 05:01:40 +0300

    ilua-wrapper.sh: use realpath instead of readlink
    * should fix running from arbitrary directories

--- ilua-wrapper.sh
+++ ilua-wrapper.sh
@@ -4,5 +4,5 @@
 # Also, it's useful as you do not have to change the directory before invoking ILua.
 # Thirdly, if you create a symbolic link to lua in a Python environment of ILua, this invoke
 # Applause by default and it will even work with Jupyter Notebooks without further tweaks.
-cd $(dirname $(readlink "$0"))
+cd $(dirname $(realpath "$0"))
 exec ./applause $APPLAUSE_OPTS "$@"
